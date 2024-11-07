0203ed1c8875b01dc8a3c529120918bc48f99931
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-11-07 19:30:04 -0300

    elaboração do html

--- exercicios/003/app.js
+++ exercicios/003/app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get("/", function(req, res) { // descreve uma rota
-    res.send('seja bem vindo')
+    res.sendFile(__dirname + '/html/index.html')
+    
+    //res.send('seja bem vindo')
 })
 
-
-
+/*
+app.get('/inf/:nome/:sexo', function(req, res) { // caminhos usando variaveis
+    res.send(`<h1>Ola ${req.params.nome}, voce e ${req.params.sexo == 'masculino' ? "Macho" : "Femia"}</h1>`)
+}) // so pode enviar um send
+*/
 
 app.listen(8080, function() {
     console.log('servidor rodando em localhost:8080')
