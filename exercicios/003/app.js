87f8b020a5ae8c11efb627bb756b071e0d1c2d2d
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-11-07 19:17:23 -0300

    Update app.js

--- exercicios/003/app.js
+++ exercicios/003/app.js
@@ -1,2 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get("/", function(req, res) { // descreve uma rota
+    res.send('seja bem vindo')
+})
+
+
+
+
+app.listen(8080, function() {
+    console.log('servidor rodando em localhost:8080')
+})
+
+
+
 // instalado o npm e express
 // npm install express --save
