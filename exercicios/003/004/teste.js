14ed80757ac682115233d9bbd33a82c685df8782
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-11-08 18:42:02 -0300

    Update teste.js

--- exercicios/003/004/teste.js
+++ exercicios/003/004/teste.js
@@ -4,8 +4,33 @@ const sequelize = new Sequelize('sistemadecadastro', 'root', '35xqnaqw', {
     dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function() {
-    console.log('conectado')
-}).catch(function() {
-    console.log('falha')
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 })
+
+Postagem.create({
+    titulo: 'teste de titulo',
+    conteudo: 'dfnsedofi dfoidfoisdjfsõdifj idsjf~sidjf'
+})
+
+const  Ususario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+//Ususario.sync({force: true})
